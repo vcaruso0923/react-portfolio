128aa00effbc721fc4c7880a59013ea36d421759
vcaruso <vinnycar0923@gmail.com>
2020-10-01 15:25:17 -0700

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
@@ -6,15 +6,39 @@ import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
 
+
+
 function App() {
+  
+  const [aboutSelected, setAboutSelected] = useState(true);
+  const [portfolioSelected, setPortfolioSelected] = useState(false);
+  const [contactSelected, setContactSelected] = useState(false);
+  const [resumeSelected, setResumeSelected] = useState(false);
+  
+  var currentPage;
+  
+  if (aboutSelected) {
+    currentPage = <About></About>
+  } else if (portfolioSelected) {
+    currentPage = <Portfolio></Portfolio>
+  } else if (contactSelected) {
+    currentPage = <Contact></Contact>
+  } else if (resumeSelected) {
+    currentPage = <Resume></Resume>
+  }
+
   return (
     <div>
-      <Nav></Nav>
-      <About></About>
-      <Portfolio></Portfolio>
-      <Contact></Contact>
-      <Resume></Resume>
-      <Footer></Footer>
+      <>
+        <Nav
+          setAboutSelected={setAboutSelected}
+          setPortfolioSelected={setPortfolioSelected}
+          setContactSelected={setContactSelected}
+          setResumeSelected={setResumeSelected}
+        ></Nav>
+        {currentPage}
+        <Footer></Footer>
+      </>
     </div>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,59 @@
 import React from 'react';
 
-function Nav() {
+function Nav(props) {
+    const {
+        // contactSelected,
+        // contactSelected,
+        // contactSelected,
+        // contactSelected,
+        setAboutSelected,
+        setPortfolioSelected,
+        setContactSelected,
+        setResumeSelected,
+    } = props;
 
     return (
-<header>
-    <h2>
-        Vincent Caruso
+        <header>
+            <h2>
+                Vincent Caruso
     </h2>
-    <nav>
-        <ul>
-            <li>
-                <a href="/">About</a>
-            </li>
-            <li>
-                <a href="/">Portfolio</a>            
-            </li>
-            <li>
-                <a href="/">Contact Me</a>               
-            </li>
-            <li>
-                <a href="/">Resume</a>                
-            </li>
-        </ul>
-    </nav>
-</header>
+            <nav>
+                <ul>
+                    <li>
+                        <span onClick={() => {
+                            setAboutSelected(true);
+                            setPortfolioSelected(false);
+                            setContactSelected(false);
+                            setResumeSelected(false);
+                        }}>About</span>
+                    </li>
+                    <li>
+                        <span onClick={() => {
+                            setAboutSelected(false);
+                            setPortfolioSelected(true);
+                            setContactSelected(false);
+                            setResumeSelected(false);
+                        }}>Portfolio</span>
+                    </li>
+                    <li>
+                        <span onClick={() => {
+                            setAboutSelected(false);
+                            setPortfolioSelected(false);
+                            setContactSelected(true);
+                            setResumeSelected(false);
+                        }}>Contact Me</span>
+                    </li>
+                    <li>
+                        <span onClick={() => {
+                            setAboutSelected(false);
+                            setPortfolioSelected(false);
+                            setContactSelected(false);
+                            setResumeSelected(true);
+                        }}>Resume</span>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
