49fb08f46c706b027bc8615f9f830b1d60c1e1a7
vcaruso <vinnycar0923@gmail.com>
2020-10-01 16:22:28 -0700

    renders all projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,52 +6,56 @@ function Projects() {
         {
             name: 'COVID-19 Data Hub',
             repo: 'https://github.com/tedpedersen/group-project-uc-berkeley',
-            link: 'https://cvdatahub.com/',
-            imagePath: '../../assets/images/covid-data.PNG',
+            link: '=https://cvdatahub.com/',
+            imagePath: `covid-data`,
         },
         {
             name: 'CA Critter Tracker',
             repo: 'https://github.com/vcaruso0923/california-critter-tracker',
             link: 'https://pure-scrubland-55089.herokuapp.com/',
-            imagePath: '../../assets/images/critter-tracker.PNG',        
+            imagePath: 'critter-tracker',
         },
         {
             name: 'Oh Snap!',
             repo: 'https://github.com/vcaruso0923/photo-port',
             link: 'https://vcaruso0923.github.io/photo-port/',
-            imagePath: '../../assets/images/oh-snap.PNG',        
+            imagePath: 'oh-snap',
         },
         {
             name: 'Food Festival',
             repo: 'https://github.com/vcaruso0923/food-festival',
             link: 'https://vcaruso0923.github.io/food-festival/',
-            imagePath: '../../assets/images/food-festival.PNG',
+            imagePath: 'food-festival',
         },
         {
             name: 'Pizza Hunt',
             repo: 'https://github.com/vcaruso0923/pizza-hunt',
             link: 'https://radiant-forest-06641.herokuapp.com/',
-            imagePath: '../../assets/images/pizza-hunt.PNG',
+            imagePath: 'pizza-hunt',
         },
         {
             name: 'Weather Dashboard',
             repo: 'https://github.com/vcaruso0923/weather-dashboard',
             link: 'https://vcaruso0923.github.io/weather-dashboard/',
-            imagePath: '../../assets/images/weather-dashboard.PNG',
+            imagePath: 'weather-dashboard',
         }
     ]
 
     return (
         <div>
-            <div>
-                <img
-                    src={require(`../../assets/images/logo192.png`)}
-                    alt='alt'
-                    key='uniqu key'
-                />
-                <h3>Project Titles</h3>
-                <a href="/">Github Links</a>
-            </div>
+            {photos.map(imageInfo => (
+                <div>
+                    <a href={imageInfo.link}>
+                        <img
+                            src={require(`../../assets/images/${imageInfo.imagePath}.PNG`)}
+                            alt={imageInfo.name}
+                            key={imageInfo.name}
+                        />
+                    </a>
+                    <h3>{imageInfo.name}</h3>
+                    <a href={imageInfo.repo}>Github Links</a>
+                </div>
+            ))}
         </div>
     );
 }
