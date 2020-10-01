0945668dcd0ddd41150e8565385b3bd7d03bb9a9
vcaruso <vinnycar0923@gmail.com>
2020-10-01 14:14:53 -0700

    portfolio/projects plain text

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,7 +4,15 @@ function Projects() {
 
     return (
         <div>
-            Projects
+            <div>
+                <img
+                    src={require(`../../assets/images/logo192.png`)}
+                    alt='alt'
+                    key='uniqu key'
+                />
+                <h3>Project Titles</h3>
+                <a href="/">Github Links</a>
+            </div>
         </div>
     );
 }
