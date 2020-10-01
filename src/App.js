6dcefa717b1eed92b6f3b1822be069b8839ad033
vcaruso <vinnycar0923@gmail.com>
2020-10-01 13:48:20 -0700

    barebones of components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav></Nav>
+      <About></About>
+      <Portfolio></Portfolio>
+      <Contact></Contact>
+      <Resume></Resume>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Projects from '../Projects';
+
+function Portfolio() {
+
+    return (
+        <div>
+            Portfolio
+            <Projects/>
+        </div>
+    );
+}
+
+export default Portfolio;
