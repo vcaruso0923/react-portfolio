bfd516bd52c323c66ad127f468ad257b03793e91
vcaruso <vinnycar0923@gmail.com>
2020-10-01 14:20:08 -0700

    About plain text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,15 @@ function About() {
 
     return (
         <div>
-            About
+            <h2>About Me</h2>
+            <img
+                src={require(`../../assets/images/pro.jpg`)}
+                alt='Vincent Caruso on the beach'
+                key='pro-pic'
+            />
+            <p>
+            I'm a full-stack web developer. Have a look at some projects I've worked on. Feel free to reach out!
+            </p>
         </div>
     );
 }
