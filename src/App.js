8cc5c7f0689f3de39b9f0c95c09968ff8c297baf
vcaruso <vinnycar0923@gmail.com>
2020-10-01 15:37:39 -0700

    update page title for each component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
@@ -9,23 +9,30 @@ import Footer from './components/Footer';
 
 
 function App() {
-  
+
   const [aboutSelected, setAboutSelected] = useState(true);
   const [portfolioSelected, setPortfolioSelected] = useState(false);
   const [contactSelected, setContactSelected] = useState(false);
   const [resumeSelected, setResumeSelected] = useState(false);
-  
+
   var currentPage;
-  
+  var pageName;
+
   if (aboutSelected) {
     currentPage = <About></About>
+    pageName = "Vincent Caruso"
   } else if (portfolioSelected) {
     currentPage = <Portfolio></Portfolio>
+    pageName = "Portfolio"
   } else if (contactSelected) {
     currentPage = <Contact></Contact>
+    pageName = "Contact"
   } else if (resumeSelected) {
     currentPage = <Resume></Resume>
+    pageName = "Resume"
   }
+
+  useEffect(() => {document.title = pageName;})
 
   return (
     <div>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,6 +5,7 @@ function Resume() {
     return (
         <div>
             <a href="../../assets/documents/VincentCarusoResume.docx" download="Vincent Caruso's Resume.docx">Download My Resume</a>
+            <p>List of Proficiencies goes here!</p>
         </div>
     );
 }
