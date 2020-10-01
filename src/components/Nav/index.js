b4d5eb624100e8f598cdfb8fb44f8b70a8a81e38
vcaruso <vinnycar0923@gmail.com>
2020-10-01 13:55:40 -0700

    nav component plain text

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,9 +3,27 @@ import React from 'react';
 function Nav() {
 
     return (
-        <div>
-            Nav
-        </div>
+<header>
+    <h2>
+        Vincent Caruso
+    </h2>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">About</a>
+            </li>
+            <li>
+                <a href="/">Portfolio</a>            
+            </li>
+            <li>
+                <a href="/">Contact Me</a>               
+            </li>
+            <li>
+                <a href="/">Resume</a>                
+            </li>
+        </ul>
+    </nav>
+</header>
     );
 }
 
