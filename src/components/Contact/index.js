5a42ac2fe94c1c9f08172efbc4bd40703f871bef
vcaruso <vinnycar0923@gmail.com>
2020-10-03 10:01:40 -0700

    resume styles

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -34,7 +34,7 @@ function ContactForm() {
 
     return (
         <section id="contact-section">
-            <h1>Contact me.</h1>
+            <h2 className="custom-h2" >Contact me</h2>
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,10 +3,39 @@ import React from 'react';
 function Resume() {
 
     return (
-        <div>
-            <a href="VincentCarusoResume.docx" download="Vincent Caruso's Resume.docx">Download My Resume</a>
-            <p>List of Proficiencies goes here!</p>
-        </div>
+        <div id="resume-section">
+            <h2 id="pro-header" className="custom-h2">Proficiencies</h2>
+            <div id="pro-divider"></div>
+            <div id="proficiencies-container">
+                <div>
+                    <h3 className="custom-h3">Front End:</h3>
+                    <ul className="proficiencies-list">
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>jQuery</li>
+                        <li>Responsive Design</li>
+                        <li>React</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3 className="custom-h3">Back End:</h3>
+                    <ul className="proficiencies-list">
+                        <li>APIs</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>MySQL (Sequelize)</li>
+                        <li>MongoDB</li>
+                        <li>REST</li>
+                        <li>GraphQL</li>
+                    </ul>
+                </div>
+            </div>
+            <div id="resume-link-container">
+                <a id="resume-link" className="custom-button" href="VincentCarusoResume.docx" download="Vincent Caruso's Resume.docx">Download My Resume!</a>
+            </div>
+        </div >
     );
 }
 
